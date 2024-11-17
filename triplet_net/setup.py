2c43fe264bdca31f1dd3edd42a7063217dc94c40
eislamoglu <a@a.c>
2024-11-17 20:09:58 +0300

    triplet net cleaned up hopefully finally

---
+++ triplet_net/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='triplet-net',
+    version='0.0.10',
+    install_requires=[
+        "torch",
+        "pandas",
+        "seaborn"
+    ],
+)
